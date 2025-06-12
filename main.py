c66156e6f5376c69cdcf07cef74d44f9afd7cd65
Hey! it's Prajwal <115614546+dumbrep@users.noreply.github.com>
2025-06-12 20:37:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -362,3 +362,7 @@ def getJobs(resume : UploadFile = File(...)):
     jobs_json= jobs.json()
     print(jobs_json["data"])
     return {"jobs" : jobs_json.get("data")}
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("your_filename:app", host="0.0.0.0", port=8080)
